cf166b1c8b51d7dfcde84dfb9f0950bfbbe8cd63
nick101992 <nick_1992@hotmail.it>
2022-11-21 16:19:37 +0100

    aggiunti esercizi

---
+++ Stampa_max.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Stampa_max {
+    public static void main (String[] args){	
+    	maxNumber();
+    }
+    
+    public static double maxNumber(){
+    	Scanner input = new Scanner(System.in);
+    	Scanner input2 = new Scanner(System.in);
+    	System.out.println("Inserisci a:");
+    	double a = input.nextDouble();
+    	System.out.println("Inserisci b:");
+    	double b = input2.nextDouble();
+    	input.close();
+    	input2.close();
+    	if(a>b) {
+    		System.out.println("Il numero "+a+" e' il piu' grande");
+    		return a;
+    	}
+    	else {
+    		System.out.println("Il numero "+b+" e' il piu' grande");
+    		return b;
+    	}
+    }
+}
--- Stampa_nome.java
+++ Stampa_nome.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 public class Stampa_nome {
-    public static void main (String[] args){   
-
+    public static void main (String[] args){
+    	
+    	System.out.println("Immetti il tuo nome:");
+    	Scanner input = new Scanner(System.in);
+    	String nome = input.next();
+    	input.close();
+    
+    	System.out.println("+--------+");
+		System.out.println("+        +");
+		System.out.println("+ "+nome+"  +");
+		System.out.println("+        +");
+		System.out.println("+--------+");
     }
 }
