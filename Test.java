5c6f268493e29155fa288a0342ba6878292a59a4
nick101992 <nick_1992@hotmail.it>
2022-11-21 13:49:14 +0100

    esercizio aggiornato

--- Test.java
+++ Test.java
@@ -1,19 +1,43 @@
+import java.util.*;
 
 public class Test {
     public static void main (String[] args){
-        int x = 0;
+    	
+    	
+    	System.out.print("Immetti numero della serie Fibonacci:");
+    	Scanner input = new Scanner(System.in);
+    	int n = input.nextInt();
+    	input.close();
+    	
+    	if(n <= 0){
+    		System.out.print("Immetti un numero maggiore o uguale ad 1");
+    	}
+    	int x = 0;
+    	
+    	if(n == 1){
+    		System.out.print(x);
+    	}
         int y = 1;
-        System.out.print(x+",");
-        System.out.print(y+",");
-        for(int z=0; z<5 ; z++) {
-        	int somma = x+y;
-        	x = y;
-        	y = somma;
-        	if(z<4){
-        		System.out.print(y+",");
-        	}
+        
+        if(n >= 2){
+        	System.out.print(x+",");
+        	if(n == 2)
+        	System.out.print(y);
         	else
-        		System.out.print(y);
+        	System.out.print(y+",");
+    	}
+        
+        if(n > 2){
+	        for(int z=0; z<n ; z++) {
+	        	int somma = x+y;
+	        	x = y;
+	        	y = somma;
+	        	if(z < (n-1)){
+	        		System.out.print(y+",");
+	        	}
+	        	else
+	        		System.out.print(y);
+	        }
         }
     }
 }
