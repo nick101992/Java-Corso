93525cc6140e9508486738798af3b6743e51f50c
nick101992 <69108212+nick101992@users.noreply.github.com>
2022-11-21 12:40:13 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,11 +1,18 @@
-import java.util.*;
-
 public class Test {
     public static void main (String[] args){
-        System.out.println("Immetti un intero ");
-        Scanner input = new Scanner (System.in);
-        int x = input.nextInt();
-        input.close();
-        System.out.println("Ho letto: " + x);
+        int x = 0;
+        int y = 1;
+        System.out.print(x+",");
+        System.out.print(y+",");
+        for(int z=0; z<5 ; z++) {
+        	int somma = x+y;
+        	x = y;
+        	y = somma;
+        	if(z<4){
+        		System.out.print(y+",");
+        	}
+        	else
+        		System.out.print(y);
+        }
     }
 }
