e567f641aaa9e3fc113f8347650982b237fa3c2f
nick101992 <nick_1992@hotmail.it>
2022-11-22 10:36:17 +0100

    Aggiornato esercizio descrescente

--- Decrescente.java
+++ Decrescente.java
@@ -10,35 +10,35 @@ public class Decrescente {
     	System.out.println("Immetti un numero di 5 cifre:");
     	Scanner input = new Scanner(System.in);
     	int n = input.nextInt();
-    	
-    	if(n>9999 && n<100000) {
-        	while(n >= 150) {
-            	System.out.println(n);	
-            	n = n-2;
+    	int k = n;
+    	if(k>9999 && k<100000) {
+        	while(k >= 150) {
+            	System.out.println(k);	
+            	k = k-2;
+                input.close();
             }    		
     	}else {
     		System.out.println("Il numero non e' valido");
     	}
     	
     	boolean flag = true;
-    	while(flag) {
-    		if(n<=9999 || n>100000) {
-    		System.out.println("Immetti un numero di 5 cifre:");
-    		Scanner input2 = new Scanner(System.in);
-    		int x = input2.nextInt();
-	    		if(x>9999 && x<100000) {
-	    			flag = false;
-	    			while(n >= 150) {
-	                	System.out.println(n);	
-	                	n = n-2;
-	            	}
-	    		}else {
-	        		System.out.println("Il numero non e' valido");
-	        		input2.close();
-	    		}
-    		}
-    		
+        if(n<=9999 || n>100000) {
+            while(flag) {
+                System.out.println("Immetti un numero di 5 cifre:");
+                Scanner input2 = new Scanner(System.in);
+                int x = input2.nextInt();
+                if(x>9999 && x<100000) {
+                    flag = false;
+                    while(x >= 150) {
+                        System.out.println(x);	
+                        x = x-2;
+                        input.close();
+                        input2.close();
+                    }
+                }else {
+                    System.out.println("Il numero non e' valido");		
+                }
+            }
     	}
-    	input.close();
    }
-}  
+}
