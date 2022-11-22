531906b06558e39936003da62571c86299f679ca
nick101992 <nick_1992@hotmail.it>
2022-11-22 10:18:22 +0100

    esercizio sommatrice aggiunto + update

--- Decrescente.java
+++ Decrescente.java
@@ -6,14 +6,39 @@ public class Decrescente {
     }
     
     public static void stampaNumDecrescenti(){
-    	System.out.print("Immetti un numero di 5 cifre:");
+    	
+    	System.out.println("Immetti un numero di 5 cifre:");
     	Scanner input = new Scanner(System.in);
     	int n = input.nextInt();
+    	
+    	if(n>9999 && n<100000) {
+        	while(n >= 150) {
+            	System.out.println(n);	
+            	n = n-2;
+            }    		
+    	}else {
+    		System.out.println("Il numero non e' valido");
+    	}
+    	
+    	boolean flag = true;
+    	while(flag) {
+    		if(n<=9999 || n>100000) {
+    		System.out.println("Immetti un numero di 5 cifre:");
+    		Scanner input2 = new Scanner(System.in);
+    		int x = input2.nextInt();
+	    		if(x>9999 && x<100000) {
+	    			flag = false;
+	    			while(n >= 150) {
+	                	System.out.println(n);	
+	                	n = n-2;
+	            	}
+	    		}else {
+	        		System.out.println("Il numero non e' valido");
+	        		input2.close();
+	    		}
+    		}
+    		
+    	}
     	input.close();
-  	
-    	while(n >= 150) {
-    	System.out.println(n);	
-    	n = n-2;
-    	} 
-    }
+   }
 }  
---
+++ Sommatrice.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Sommatrice {
+    public static void main (String[] args){	
+    	somma();
+    }
+    
+    public static void somma(){
+    	boolean flag=true;
+    	double x = 0;
+    	
+    	while(flag) {
+    		@SuppressWarnings("resource")
+	    	Scanner input = new Scanner(System.in);
+	    	System.out.println("Inserisci un numero:");
+	    	double a = input.nextDouble();
+	    	x=x+a;
+	    	System.out.println("La somma dei numeri inseriti e':"+x);
+	    	if(a==0) {
+	    		System.out.println("Programma chiuso");
+	    		input.close();
+	    		break;
+	    	}
+    	}
+    }
+}
