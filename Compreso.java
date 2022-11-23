13e69a806220badac9e70f733d4502ea422173bf
nick101992 <nick_1992@hotmail.it>
2022-11-23 12:14:13 +0100

    aggiunti esercizi

--- Compreso.java
+++ Compreso.java
@@ -1,2 +1,30 @@
 /*Inserire tre numeri A, B e C dire se B è compreso
 tra A e C*/
+
+import java.util.Scanner;
+public class Compreso {
+    public static void main (String[] args){	
+    	maxNumber();
+    }
+    
+    public static void maxNumber(){
+    	Scanner input = new Scanner(System.in);
+    	Scanner input2 = new Scanner(System.in);
+    	Scanner input3 = new Scanner(System.in);
+    	System.out.println("Inserisci a:");
+    	double a = input.nextDouble();
+    	System.out.println("Inserisci b:");
+    	double b = input2.nextDouble();
+    	System.out.println("Inserisci c:");
+    	double c = input3.nextDouble();
+    	input.close();
+    	input2.close();
+    	input3.close();
+    	if((b <= a && b >= c) || (b >= a && b < c)) {
+    		System.out.println("Il numero b "+b+" e' compreso tra a e b");
+    	}
+    	else {
+    		System.out.println("Il numero b "+b+" non e' compreso tra a e b");
+    	}
+    }
+}
--- Funzione.java
+++ Funzione.java
@@ -2,7 +2,6 @@
 alla fine restituisca il maggiore (Per finire il programma inserire 0)*/
 
 import java.util.Scanner;
-import java.util.Collection;
 public class Funzione {
     public static void main (String[] args){	
     	somma();
@@ -10,28 +9,23 @@ public class Funzione {
     
     public static void somma(){
     	boolean flag=true;
-    	int n = 0;
-    	List<E> l;
+    	double maxNum = 0;
     	while(flag) {
     		@SuppressWarnings("resource")
 	    	Scanner input = new Scanner(System.in);
 	    	System.out.println("Inserisci un numero:");
 	    	double a = input.nextDouble();
-	    	n=n+1;
-	    	System.out.println(n);
-	    	listaNumeri[0]=a;
-	    	System.out.println(listaNumeri[0]);
-	    	double maxNum = listaNumeri[0];
-	    	for(double k : listaNumeri) {
-	    		if(k<maxNum){
-	    		maxNum=k;
-	    		}
+	    	if(a<maxNum && a!=0){
+	    		System.out.println("Il numero massimo e':"+maxNum);
 	    	}
-	    	System.out.println("Il numero massimo è:"+maxNum);
-	    	if(a==0) {
+	    	else if(a==0){
 	    		System.out.println("Programma chiuso");
 	    		input.close();
 	    		break;
+	    	}
+	    	else {
+	    		System.out.println("Il numero massimo e':"+a);
+	    		maxNum = a;
 	    	}
     	}
     }
